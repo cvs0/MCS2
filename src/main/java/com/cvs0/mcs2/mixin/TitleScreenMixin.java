83de1ff7f180ecc9a9907fc144d6a572fc76c194
cvs0 <143862758+cvs0@users.noreply.github.com>
2023-10-01 22:18:16 -0600

    Create TitleScreenMixin.java

---
+++ src/main/java/com/cvs0/mcs2/mixin/TitleScreenMixin.java
@@ -0,0 +1,8 @@
+package com.cvs0.mcs2.mixin;
+
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(TitleScreen.class)
+public class TitleScreenMixin {
+}
